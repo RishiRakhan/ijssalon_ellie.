cd5cea9fd15c5846a8d307ec452f7fd0bed72e70
Rishi <rishirakhan@hotmail.com>
2025-11-18 13:25:18 +0100

    Initial commit - IJssalon homework

--- static/tijdelijk.py
+++ static/tijdelijk.py
@@ -1,26 +1,28 @@
-prijzen = {
-    'aardbei': 3,
-    'vanille': 4,
-    'chocholade': 5
-}
+def print_aanbieding():
+    prijzen = {
+        'aardbei': 3,
+        'vanille': 4,
+        'chocolade': 5
+    }
 
+    aanbieding = prijzen["aardbei"] * 0.8
 
+    reclame_tekst = f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
 
-aanbieding = prijzen['aardbei'] * 8
+    reclame_tekst2 = reclame_tekst[:63]
 
+    reclame_tekst3 = reclame_tekst2.upper()
 
-reclame_teskt = f"vandaag in aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst4 = reclame_tekst3.split()
 
-reclame_teskt2 = reclame_teskt[:62]
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
 
-reclame_teskt3 = reclame_teskt2.upper()
-
-reclame_teskt4 = reclame_teskt3.split()
-
-
-for el in reclame_teskt4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el.lower())
+print_aanbieding()
         
+
+
+
